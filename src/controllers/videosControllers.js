fd3f6665b8f7367098ca74550929187409ef6f92
rlatlswo135 <rlatlswo13@gmail.com>
2022-05-16 01:05:00 +0900

    pug

--- src/controllers/videosControllers.js
+++ src/controllers/videosControllers.js
@@ -1,8 +1,48 @@
+const fakeUser = {
+    userName:'kim',
+    login:true
+}
+
 export function homepage(req,res){
-    return res.send('homepage -> show videos')
+    const videos = [
+        {
+            title:"Video #1",
+            rating:3,
+            comments:50,
+            createdAt:'2015-05-03',
+            views:100
+        },
+        {
+            title:"Video #2",
+            rating:5,
+            comments:100,
+            createdAt:'2018-05-03',
+            views:130
+        },
+        {
+            title:'Video #3',
+            rating:1,
+            comments:10,
+            createdAt:'2012-05-03',
+            views:50
+        }
+    ]
+    // const videos = []
+    
+    return res.render("home",{
+        pageTitle:"home",
+        user:fakeUser,
+        videos
+    })
+    /*
+    what is res.render? => express봐서 인자랑 다 보자
+    컨트롤러가 home.pug를 렌더하고있자너?
+    그럼 여기서 변수를 보내줘야겠지? => 2번째인자
+    사용하는 render페이지에서 봐보자 -> home.pug겠지?
+    => #{}안에 Js코드 쓰는데 그안에 변수로 들어가있는모습.
+    */
 }
 export function edit(req,res){
-    console.log(req.params)
     return res.send('videos -> edit')
 }
 export function search(req,res){
@@ -15,13 +55,13 @@ export function watch(req,res){
     // if(!findNumber){
     //     return res.send('video id is number')
     // }
-    return res.send(`videos -> watch #${req.params.id}`)
+    return res.render("watch",{
+        id:req.params.id,
+    })
 }
 export function remove(req,res){
-    console.log(req.params)
     return res.send(`videos -> remove`)
 }
 export function upload(req,res){
-    console.log(req.params)
     return res.send(`videos -> upload`)
 }
--- src/server.js
+++ src/server.js
@@ -1,22 +1,31 @@
 import express from 'express'; //npm+nodejs가 똑똑하니께 알아서 node_module에서 express를 찾아주겟지
 import morgan from 'morgan';
-// morgan함수를 호출하면. 내가 설정한대로 middleware를 return해준다.
 import globalRouter from './routers/globalRouter';
 import usersRouter from './routers/usersRouter';
 import videosRouter from './routers/videosRouter';
-//export-import
 
 const PORT = 4000;
 const app = express();
-app.use(morgan('dev'))
-//morgan을 쓰니까 서버콘솔에 httpMethod,path,statuCode 속도 등의 정보가 나온다.
-//middleware랑 같지? 얘를 거쳤으니 서버콘솔에 위의 정보가나오는거고 결국 각 handle함수로 넘어가잖어.
 
+//use pug -> 사용은 express.set부분에
+app.set('view engine','pug')
+//1. pug설치 2. pug를 뷰엔진으로 3. pug파일 생성
+//process.cwd() => process(현재실행중인 프로그램) + cwd(currently working directory)
+// + /views
+// console.log(process.cwd()) -> /side_project/wetube (src가아니고?)
+/*
+현재실행중인 경로는 서버를 실행시킨 즉 node index.js한것처럼 그렇게 서버를 실행시킨(노드를 실행시킨)
+녀석의 디렉토리를 가르킬텐데 npm run dev -> package.json이 실행하지?
+그러니까 package.json의 디렉토리인 wetube까지만 되는거
+*/
+
+app.set('views',process.cwd()+'/src/views');
+//그래서 디렉토리를 수정해준모습 express 공식에 프로퍼티(view,view engine등)별 설명이 나와있다
+app.use(morgan('dev'))
 
 app.use('/',globalRouter);
 app.use('/users',usersRouter);
 app.use('/videos',videosRouter);
-
 
 
 
